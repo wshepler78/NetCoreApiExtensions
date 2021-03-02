caf1fd85a692d279bdf7869fc400594d1f9ce8d0
Will Shepler <wills@mycoitracking.com>
2021-03-02 16:49:39 -0500

    Modified Sorting Extensions
    Updated sorting extensions to support the sort direction directly and extend the native EF ordering extensions.

--- NetCoreApiExtensions.EntityFramework/Sorting/Extensions.cs
+++ NetCoreApiExtensions.EntityFramework/Sorting/Extensions.cs
@@ -7,50 +7,8 @@ namespace NetCoreApiExtensions.EntityFramework
 {
     public static partial class Extensions
     {
-        public static IOrderedQueryable<T> OrderBy<T, TKey>(this IQueryable<T> query, Tuple<Expression<Func<T, TKey>>, SortDirection> sortTuple)
-        {
-            return query.OrderBy(sortTuple.Item1, sortTuple.Item2);
-        }
-
-        public static IOrderedQueryable<T> OrderBy<T, TKey>(this IQueryable<T> query, params Tuple<Expression<Func<T, TKey>>, SortDirection>[] sortTuples)
-        {
-            if (sortTuples.Length == 0)
-            {
-                throw new Exception("At least one sort must be specified");
-            }
-
-            var expressions = sortTuples.Select(st => new SortExpression<T, TKey>(st.Item1, st.Item2));
-
-            return query.OrderBy(expressions.ToArray());
-        }
-
-        public static IOrderedQueryable<T> OrderBy<T, TKey>(this IQueryable<T> query, SortExpression<T, TKey> sortExpression)
-        {
-            return query.OrderBy(sortExpression.KeySelector, sortExpression.SortDirection);
-        }
-
-        public static IOrderedQueryable<T> OrderBy<T, TKey>(this IQueryable<T> query, params SortExpression<T, TKey>[] sortExpressions)
-        {
-            if (sortExpressions.Length == 0)
-            {
-                throw new Exception("At least one sort must be specified");
-            }
-
-            foreach (var sortExpression in sortExpressions)
-            {
-                query = query.OrderBy(new SortExpression<T, TKey>(sortExpression.KeySelector, sortExpression.SortDirection));
-            }
-
-            return (IOrderedQueryable<T>)query;
-        }
-
         public static IOrderedQueryable<T> OrderBy<T, TKey>(this IQueryable<T> query, Expression<Func<T, TKey>> keySelector, SortDirection direction)
         {
-            if (query is IOrderedQueryable<T> orderedQuery)
-            {
-                return orderedQuery.ThenBy(keySelector, direction);
-            }
-
             return direction switch
                    {
                        SortDirection.Asc => query.OrderBy(keySelector),
