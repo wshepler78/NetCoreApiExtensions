6dfdafe3a81f1872a4df88baccfeb22e76a49f0e
Will Shepler <wshepler78@users.noreply.github.com>
2023-02-02 10:23:39 -0500

    Added extension method to convert to StatusCodeException from Exception

--- NetCoreApiExtensions.Shared/Extensions/ExceptionExtensions.cs
+++ NetCoreApiExtensions.Shared/Extensions/ExceptionExtensions.cs
@@ -5,6 +5,11 @@ namespace NetCoreApiExtensions.Shared.Extensions
 {
     public static class ExceptionExtensions
     {
+        /// <summary>
+        /// Unrolls the exception into a single string
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        /// <returns>System.String.</returns>
         public static string GetFullExceptionString(this Exception exception)
         {
             var exceptionDetails = new StringBuilder();
@@ -21,6 +26,21 @@ namespace NetCoreApiExtensions.Shared.Extensions
             }
 
             return exceptionDetails.ToString();
+        }
+
+        /// <summary>
+        /// Throws a StatusCodeException with the specified status code
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        /// <param name="statusCode">The status code.</param>
+        /// <param name="errors"></param>
+        /// <param name="keyedErrors"></param>
+        /// <returns>StatusCodeException.</returns>
+        public static StatusCodeException ThrowStatusCodeException(this Exception exception, HttpStatusCode statusCode,
+            IEnumerable<string> errors = null, IEnumerable<IListItem<string, string>> keyedErrors = null)
+        {
+            var statusCodeException = new StatusCodeException(statusCode, exception.Message, exception.InnerException, errors, keyedErrors);
+            throw statusCodeException;
         }
     }
 }
